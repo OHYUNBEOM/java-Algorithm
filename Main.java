60a64c963c14512f41afa6c4f493dce03a917dd3
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-10-21 02:27:23 +0900

    Bridge

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-import pagemaker.PageMaker;
-
 public class Main {
     public static void main(String[] args) {
-        PageMaker.makeLinkPage("linkpage.html");
+        IncreaseDisplay d1 = new IncreaseDisplay(new CharDisplayImpl('<', '*', '>'), 1);
+        IncreaseDisplay d2 = new IncreaseDisplay(new CharDisplayImpl('|', '#', '-'), 2);
+        d1.increaseDisplay(4);
+        d2.increaseDisplay(6);
     }
 }
