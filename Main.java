c28174041d71309075a4bd7d98a500243a66ee72
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-11-05 00:08:14 +0900

    Visitor

--- Main.java
+++ Main.java
@@ -1,8 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        IncreaseDisplay d1 = new IncreaseDisplay(new CharDisplayImpl('<', '*', '>'), 1);
-        IncreaseDisplay d2 = new IncreaseDisplay(new CharDisplayImpl('|', '#', '-'), 2);
-        d1.increaseDisplay(4);
-        d2.increaseDisplay(6);
+        try {
+            System.out.println("Making root entries...");
+            Directory rootdir = new Directory("root");
+            Directory bindir = new Directory("bin");
+            Directory tmpdir = new Directory("tmp");
+            Directory usrdir = new Directory("usr");
+            rootdir.add(bindir);
+            rootdir.add(tmpdir);
+            rootdir.add(usrdir);
+            bindir.add(new File("vi", 10000));
+            bindir.add(new File("latex", 20000));
+            rootdir.accept(new ListVisitor());             
+
+            System.out.println("");
+            System.out.println("Making user entries...");
+            Directory Kim = new Directory("Kim");
+            Directory Lee = new Directory("Lee");
+            Directory Kang = new Directory("Kang");
+            usrdir.add(Kim);
+            usrdir.add(Lee);
+            usrdir.add(Kang);
+            Kim.add(new File("diary.html", 100));
+            Kim.add(new File("Composite.java", 200));
+            Lee.add(new File("memo.tex", 300));
+            Kang.add(new File("game.doc", 400));
+            Kang.add(new File("junk.mail", 500));
+            rootdir.accept(new ListVisitor());    
+            System.out.println("");
+            System.out.println("NumberOfFileVisitor Ãß°¡");
+            rootdir.accept(new NumberOfFileVisitor());
+        } catch (FileTreatmentException e) {
+            e.printStackTrace();
+        }
     }
 }
