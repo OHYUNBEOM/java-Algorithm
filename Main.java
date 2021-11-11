71cdc1b04d5e6c90ae83b346eb45b6ed6c2a986c
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-11-11 22:58:24 +0900

    Observer

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    static public void main(String args[]) {
-        new LoginFrame("Mediator Sample");
+    public static void main(String[] args) {
+        NumberGenerator generator = new RandomNumberGenerator();
+        Observer observer1 = new DigitObserver();
+        Observer observer2 = new GraphObserver();
+        Observer observer3 = new FrameObserver();
+        generator.addObserver(observer1);
+        generator.addObserver(observer2);
+        generator.addObserver(observer3);
+        generator.execute();
     }
 }
